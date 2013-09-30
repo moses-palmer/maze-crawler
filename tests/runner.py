fbe6cb418510b3772fb549d948487c329bdc4d86
Moses Palmér <mosespalmer@gmail.com>
2013-09-30 21:01:50 +0200

    Moved test runner code into main function

--- tests/runner.py
+++ tests/runner.py
@@ -22,15 +22,22 @@ sys.path.append(
 import tests
 from tests.suites import *
 
-failures = tests.run()
-if failures is None:
-    print('Test suite was cancelled by setup')
-    sys.exit(-1)
 
-print('')
-print('All test suites completed with %d failed tests' % len(failures))
-if failures:
-    print('Failed tests:\n%s' % '\n'.join(
-        '\t%s - %s' % (test.name, test.message)
-            for test in failures))
-sys.exit(len(failures))
+def main():
+    failures = tests.run()
+    if failures is None:
+        print('Test suite was cancelled by setup')
+        sys.exit(-1)
+
+    print('')
+    print('All test suites completed with %d failed tests' % len(failures))
+    if failures:
+        print('Failed tests:\n%s' % '\n'.join(
+            '\t%s - %s' % (test.name, test.message)
+                for test in failures))
+
+    return len(failures)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
